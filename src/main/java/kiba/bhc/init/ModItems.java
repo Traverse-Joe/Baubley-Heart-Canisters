f7020b1b8a90c840e95c16a0ba0a671d794aa4d1
UpcraftLP <j-schirra@gmx.de>
2017-12-02 14:31:52 +0100

    added blue heart canister

--- src/main/java/kiba/bhc/init/ModItems.java
+++ src/main/java/kiba/bhc/init/ModItems.java
@@ -14,8 +14,10 @@ public class ModItems {
     public static final Item RED_HEART_CANISTER = new BaseHeartCanister("red", UUID.fromString("caa44aa0-9e6e-4a57-9759-d2f64abfb7d3"));
     public static final Item ORANGE_HEART_CANISTER = new BaseHeartCanister("orange", UUID.fromString("147d8bca-a33c-440e-8985-22207747ffd8"));
     public static final Item GREEN_HEART_CANISTER = new BaseHeartCanister("green", UUID.fromString("85084e13-daa0-4340-aebc-5d1e1dedc3d9"));
+    public static final Item BLUE_HEART_CANISTER = new BaseHeartCanister("blue", UUID.fromString("268edd4a-e364-4afe-8e1e-7212734a7025"));
 
     public static final Item RED_HEART = new BaseHeartItem("red", 10);
     public static final Item ORANGE_HEART = new BaseHeartItem("orange",20);
     public static final Item GREEN_HEART = new BaseHeartItem("green",30);
+    //TODO add blue heart item!
 }
