b94ae8b07aee5bec65a00b48d1592b2437f65979
UpcraftLP <j-schirra@gmx.de>
2017-12-09 17:46:49 +0100

    fixed Tinker's Construct mod integration

--- src/main/java/kiba/bhc/handler/DropHandler.java
+++ src/main/java/kiba/bhc/handler/DropHandler.java
@@ -2,50 +2,52 @@ package kiba.bhc.handler;
 
 import kiba.bhc.Reference;
 import kiba.bhc.init.ModItems;
+import kiba.bhc.proxy.CommonProxy;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.boss.EntityDragon;
 import net.minecraft.entity.monster.EntityEvoker;
 import net.minecraft.entity.monster.EntityWitherSkeleton;
 import net.minecraft.entity.monster.IMob;
 import net.minecraftforge.event.entity.living.LivingDropsEvent;
-import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
+import java.util.Random;
+
 @Mod.EventBusSubscriber(modid = Reference.MODID)
 public class DropHandler {
+
+
 
     @SubscribeEvent
     public static void onEntityDrop(LivingDropsEvent event) {
         EntityLivingBase entity = event.getEntityLiving();
         if (entity.world.isRemote) return; //no duplicate glitch on client!
         if (entity instanceof IMob) {
+            Random random = entity.getRNG();
             if (!entity.isNonBoss()) { // = is boss
                 if (entity instanceof EntityDragon) {
-                    if (entity.getRNG().nextDouble() < ConfigHandler.greenDropRate) {
+                    if (random.nextDouble() < ConfigHandler.greenDropRate) {
                         entity.dropItem(ModItems.GREEN_HEART, 1);
                     }
-
                 }
-                else if (entity.getRNG().nextDouble()<ConfigHandler.orangeDropRate){
+                else if (random.nextDouble() < ConfigHandler.orangeDropRate){
                     entity.dropItem(ModItems.ORANGE_HEART, 1);
                 }
             } else { //no boss
                 if (entity instanceof EntityEvoker) {
-                    if (entity.getRNG().nextDouble() < ConfigHandler.blueDropRate) {
+                    if (random.nextDouble() < ConfigHandler.blueDropRate) {
                         entity.dropItem(ModItems.BLUE_HEART, 1);
                     }
-
-                } else {
-                    if (entity.getRNG().nextDouble() < ConfigHandler.redDropRate) {
-                        entity.dropItem(ModItems.RED_HEART, 1);
+                }
+                else if(!CommonProxy.TINKERS_CONSTRUCT_INSTALLED && entity instanceof EntityWitherSkeleton) {
+                    if (random.nextDouble() < ConfigHandler.boneDropRate) {
+                        entity.dropItem(ModItems.WITHER_BONE, 1);
                     }
                 }
-                if (!(Loader.isModLoaded("tconstruct"))) {
-                    if (entity instanceof EntityWitherSkeleton) {
-                        if (entity.getRNG().nextDouble() < ConfigHandler.boneDropRate) {
-                            entity.dropItem(ModItems.WITHER_BONE, 1);
-                        }
+                else { // default/fallback
+                    if (random.nextDouble() < ConfigHandler.redDropRate) {
+                        entity.dropItem(ModItems.RED_HEART, 1);
                     }
                 }
             }
--- src/main/java/kiba/bhc/proxy/CommonProxy.java
+++ src/main/java/kiba/bhc/proxy/CommonProxy.java
@@ -1,13 +1,17 @@
 package kiba.bhc.proxy;
 
 import kiba.bhc.BaubleyHeartCanisters;
+import kiba.bhc.handler.DropHandler;
 import kiba.bhc.util.BHCGuiHandler;
+import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.network.NetworkRegistry;
 
 public class CommonProxy {
+
+    public static boolean TINKERS_CONSTRUCT_INSTALLED;
 
     public void preInit(FMLPreInitializationEvent event){
         NetworkRegistry.INSTANCE.registerGuiHandler(BaubleyHeartCanisters.INSTANCE, new BHCGuiHandler());
@@ -18,6 +22,6 @@ public class CommonProxy {
     }
 
     public void postInit(FMLPostInitializationEvent event) {
-
+        TINKERS_CONSTRUCT_INSTALLED = Loader.isModLoaded("tconstruct");
     }
 }
