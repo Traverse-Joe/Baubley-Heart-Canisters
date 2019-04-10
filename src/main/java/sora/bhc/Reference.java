7b91a830e3c024c538a4ccff27843919358017f1
Robert Booster <boosterdev@users.noreply.github.com>
2019-04-10 17:04:33 +0200

    Update Reference.java

--- src/main/java/sora/bhc/Reference.java
+++ src/main/java/sora/bhc/Reference.java
@@ -6,6 +6,6 @@ public class Reference {
     public static final String VERSION = "@VERSION@";
     public static final String MODNAME = "Baubley Heart Canisters";
     public static final String CLIENTPROXY = "sora.bhc.proxy.ClientProxy";
-    public static final String COMMONPROXY = "kiba.bhc.proxy.CommonProxy";
+    public static final String COMMONPROXY = "sora.bhc.proxy.CommonProxy";
     public static final String UPDATE_URL = "https://raw.githubusercontent.com/EmoKiba/Baubley-Heart-Canisters/master/versions.json";
 }
