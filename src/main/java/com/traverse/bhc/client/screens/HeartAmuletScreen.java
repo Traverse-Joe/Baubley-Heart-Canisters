99cf3e68468bebcd8764f61a905312b8dc101c67
Traverse-Joe <roxxanrebirth@gmail.com>
2021-07-29 23:40:12 -0700

    1.17 Port Complete

--- src/main/java/com/traverse/bhc/client/screens/HeartAmuletScreen.java
+++ src/main/java/com/traverse/bhc/client/screens/HeartAmuletScreen.java
@@ -1,9 +1,11 @@
 package com.traverse.bhc.client.screens;
 
+import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.traverse.bhc.common.BaubleyHeartCanisters;
 import com.traverse.bhc.common.container.HeartAmuletContainer;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Inventory;
@@ -26,9 +28,12 @@ public class HeartAmuletScreen extends AbstractContainerScreen<HeartAmuletContai
        this.renderTooltip(stack, mouseX, mouseY);
     }
 
+
     @Override
     protected void renderBg(PoseStack stack, float partialTicks, int mouseX, int mouseY) {
-        getMinecraft().getTextureManager().bindForSetup(BACKGROUND_TEXTURE);
+        RenderSystem.setShader(GameRenderer::getPositionTexShader);
+        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+        RenderSystem.setShaderTexture(0, this.BACKGROUND_TEXTURE);
         blit(stack, leftPos, topPos, 0, 0, imageWidth, imageHeight);
     }
 
