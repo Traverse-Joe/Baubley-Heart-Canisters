2c9a2d9e688459ae12e587b21a6e73f1432121c9
Traverse-Joe <roxxanrebirth@gmail.com>
2023-06-15 12:54:54 -0400

    WIP EasterEgg

--- src/main/java/com/traverse/bhc/common/util/Misc.java
+++ src/main/java/com/traverse/bhc/common/util/Misc.java
@@ -1,14 +1,11 @@
-package com.traverse.bhc.client;
+package com.traverse.bhc.common.util;
 
 import com.traverse.bhc.common.BaubleyHeartCanisters;
 import com.traverse.bhc.common.init.RegistryHandler;
 import net.minecraft.client.renderer.item.ItemProperties;
-import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraftforge.client.event.TextureStitchEvent;
-import net.minecraftforge.client.model.generators.ItemModelProvider;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -18,13 +15,13 @@ import java.util.Locale;
 import static net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus.MOD;
 
 @Mod.EventBusSubscriber(modid = BaubleyHeartCanisters.MODID, bus = MOD)
-public class ClientBaubleyHeartCanisters {
-
-    public static final ResourceLocation SLOT_TEXTURE = new ResourceLocation(BaubleyHeartCanisters.MODID, "slots/empty_heartamulet");
+public class Misc {
 
     @OnlyIn(Dist.CLIENT)
     @SubscribeEvent
-    public static void textureStitch(final TextureStitchEvent.Pre evt) {
-        evt.addSprite(ClientBaubleyHeartCanisters.SLOT_TEXTURE);
+    public static void addItemProperties(FMLClientSetupEvent event) {
+        ItemProperties.register(RegistryHandler.BLADE_OF_VITALITIY.get(), new ResourceLocation(BaubleyHeartCanisters.MODID, "darkness"), (stack, level, entity, value) -> {
+            return stack.hasCustomHoverName() && stack.getHoverName().getString().toLowerCase(Locale.ROOT).contains("beautiful eyes")? 1.0F :0.0F;
+        });
     }
 }
