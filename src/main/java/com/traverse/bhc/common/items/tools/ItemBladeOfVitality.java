e2b5a290c39f1ec44a811d1a147c36a654bebe53
Up <upcraftedlp@gmail.com>
2024-08-12 13:47:03 +0200

    bump neoforge to get latest features

--- src/main/java/com/traverse/bhc/common/items/tools/ItemBladeOfVitality.java
+++ src/main/java/com/traverse/bhc/common/items/tools/ItemBladeOfVitality.java
@@ -6,10 +6,12 @@ import com.traverse.bhc.common.BaubleyHeartCanisters;
 import com.traverse.bhc.common.container.BladeOfVitalityContainer;
 import com.traverse.bhc.common.container.HeartAmuletContainer;
 import com.traverse.bhc.common.init.RegistryHandler;
+import com.traverse.bhc.common.items.ItemHeartAmulet;
 import com.traverse.bhc.common.util.HeartType;
 import com.traverse.bhc.common.util.SoulContainerProvider;
 import net.minecraft.ChatFormatting;
 import net.minecraft.Util;
+import net.minecraft.core.Holder;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.chat.Component;
@@ -20,6 +22,7 @@ import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.MenuProvider;
 import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
@@ -36,11 +39,12 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.function.Consumer;
 import java.util.stream.IntStream;
 
 public class ItemBladeOfVitality extends SwordItem implements SoulContainerProvider {
 
-    public static final UUID DAMAGE_MODIFIER_ID = UUID.fromString("432ba3b0-c3bd-4f1c-b14c-76a0b32a386c");
+    public static final ResourceLocation DAMAGE_MODIFIER_ID = BaubleyHeartCanisters.id("blade_of_vitality");
 
 
     // TODO: make an actual Tier for Blade of Vitality Easier to Customize
@@ -66,40 +70,23 @@ public class ItemBladeOfVitality extends SwordItem implements SoulContainerProvi
     }
 
     @Override
-    public ItemAttributeModifiers getDefaultAttributeModifiers(ItemStack stack) {
-        var result = super.getDefaultAttributeModifiers(stack);
-
-        return result;
-    }
-
-    @Override
     public Multimap<Attribute, AttributeModifier> getAttributeModifiers(EquipmentSlot slot, ItemStack stack) {
         ImmutableMultimap.Builder<Attribute, AttributeModifier> RESULT = ImmutableMultimap.builder();
         RESULT.putAll(super.getAttributeModifiers(slot, stack));
         if(slot == EquipmentSlot.MAINHAND) {
-            int[] heartCount = getHeartCount(stack);
+            int[] heartCount = ItemHeartAmulet.getHeartCount(stack);
             int heartTotal = IntStream.of(heartCount).sum();
             if (heartTotal > 0) {
-                RESULT.put(Attributes.ATTACK_DAMAGE, new AttributeModifier(DAMAGE_MODIFIER_ID, "Weapon modifier", heartTotal, AttributeModifier.Operation.ADDITION));
+                RESULT.put(Attributes.ATTACK_DAMAGE, new AttributeModifier(DAMAGE_MODIFIER_ID, heartTotal, AttributeModifier.Operation.ADD_VALUE));
             }
         }
         return RESULT.build();
     }
 
-    public int[] getHeartCount(ItemStack stack) {
-        if (stack.hasTag()) {
-            CompoundTag nbt = stack.getTag();
-            if (nbt.contains(BladeOfVitalityContainer.HEART_AMOUNT))
-                return nbt.getIntArray(BladeOfVitalityContainer.HEART_AMOUNT);
-        }
-
-        return new int[HeartType.values().length];
-    }
-
-    //ToDo Actually check the length of the Hearts on the Weapon and Add to the damage
+    //TODO Actually check the length of the Hearts on the Weapon and Add to the damage
     @Override
-    public float getDamage() {
-        return this.getMaxDamage(RegistryHandler.BLADE_OF_VITALITY.get().getDefaultInstance()) + HeartAmuletContainer.HEART_AMOUNT.length();
+    public int getDamage(ItemStack stack) {
+        return getMaxDamage(stack) + HeartAmuletContainer.HEART_AMOUNT.length();
     }
 
     @Override
@@ -108,13 +95,13 @@ public class ItemBladeOfVitality extends SwordItem implements SoulContainerProvi
     }
 
     @Override
-    public void appendHoverText(ItemStack stack, Level worldIn, List<Component> tooltip, TooltipFlag flagIn) {
-        super.appendHoverText(stack, worldIn, tooltip, flagIn);
-        tooltip.add(Component.translatable(Util.makeDescriptionId("tooltip", RegistryHandler.BLADE_OF_VITALITY.getId())).setStyle(Style.EMPTY.applyFormat(ChatFormatting.GOLD)));
+    public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
+        super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
+        tooltipComponents.add(Component.translatable(Util.makeDescriptionId("tooltip", RegistryHandler.BLADE_OF_VITALITY.getId())).setStyle(Style.EMPTY.applyFormat(ChatFormatting.GOLD)));
     }
 
     @Override
-    public boolean canApplyAtEnchantingTable(ItemStack stack, Enchantment enchantment) {
+    public boolean supportsEnchantment(ItemStack stack, Holder<Enchantment> enchantment) {
         return true;
     }
 
